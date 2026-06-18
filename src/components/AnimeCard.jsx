215f284a5d7eac249ecdb115ea15591aab711323
FDaaniel524 <fernandocecyt1@gmail.com>
2026-06-17 18:26:21 -0600

    Se corrigió un bug relacionado con el enlace de descripción y el icono de favoritos

--- src/components/AnimeCard.jsx
+++ src/components/AnimeCard.jsx
@@ -4,26 +4,30 @@ function AnimeCard({ anime, onToggleFavorite, favoritos }) {
   const isFavorite = favoritos.some((fav) => fav.mal_id === anime.mal_id);
 
   return (
-    <Link to={`/anime/${anime.mal_id}`} className="anime-link">
-      <div className="anime-item">
+    <div className="anime-item">
+      <img
+        src={isFavorite ? "favorite-on.png" : "favorite-off.png"}
+        alt="Add favorite"
+        className="favorite-icon"
+        onClick={() => onToggleFavorite(anime)}
+      />
+
+      <Link to={`/anime/${anime.mal_id}`} className="anime-link">
         <img
           src={anime.images.jpg.image_url}
           alt={anime.title}
           className="anime-image"
         />
+
         <p className="anime-title">{anime.title}</p>
+
         <p className="anime-genre">
           {anime.genres?.map((genre) => genre.name).join(", ")}
         </p>
+
         <p className="anime-score">Score: {anime.score}</p>
-        <img
-          src={isFavorite ? "favorite-on.png" : "favorite-off.png"}
-          alt="Add favorite"
-          className="favorite-icon"
-          onClick={() => onToggleFavorite(anime)}
-        />
-      </div>
-    </Link>
+      </Link>
+    </div>
   );
 }
 
